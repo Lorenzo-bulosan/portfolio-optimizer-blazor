d76527c970224f4beb33f14fe172cb993e5d72ce
lorenzo bulosan <lorenzo.bulosan@yahoo.co.uk>
2025-07-04 13:15:48 +0100

    Functionality to allow single file uploads containing multiple stocks and clean UI combining stock metrics in a table

--- PortfolioOptimizer/Services/FileUtils.cs
+++ PortfolioOptimizer/Services/FileUtils.cs
@@ -1,11 +1,12 @@
-﻿using PortfolioOptimizer.Data;
+﻿// PortfolioOptimizer/Services/FileUtils.cs
+using PortfolioOptimizer.Data;
 using System.Globalization;
 
 namespace PortfolioOptimizer.Services
 {
     public static class FileUtils
     {
-        public static Stock ParseCsvSingleStock(string content)
+        public static List<Stock> ParseCsv(string content)
         {
             using var reader = new StringReader(content);
 
@@ -25,10 +26,7 @@ namespace PortfolioOptimizer.Services
             if (dateIdx == -1 || closeIdx == -1 || nameIdx == -1)
                 throw new InvalidDataException("CSV file missing required columns (date, open, close, Name).");
 
-            var stock = new Stock
-            {
-                Prices = new List<Price>()
-            };
+            var stocksDict = new Dictionary<string, Stock>();
 
             string? line;
             while ((line = reader.ReadLine()) != null)
@@ -49,7 +47,21 @@ namespace PortfolioOptimizer.Services
                 if (!decimal.TryParse(fields[closeIdx], NumberStyles.Any, CultureInfo.InvariantCulture, out decimal close))
                     continue;
 
-                // Populate Price object
+                string stockName = fields[nameIdx];
+
+                // Get or create stock
+                if (!stocksDict.ContainsKey(stockName))
+                {
+                    stocksDict[stockName] = new Stock
+                    {
+                        Name = stockName,
+                        Prices = new List<Price>()
+                    };
+                }
+
+                var stock = stocksDict[stockName];
+
+                // Create price object
                 var price = new Price
                 {
                     Date = date,
@@ -58,15 +70,46 @@ namespace PortfolioOptimizer.Services
                     Stock = stock
                 };
 
-                // Populate Stock object
-                if (string.IsNullOrWhiteSpace(stock.Name))
-                {
-                    stock.Name = fields[nameIdx];
-                }
                 stock.Prices.Add(price);
             }
 
-            return stock;
+            return stocksDict.Values.ToList();
+        }
+
+        // Keep the old method for backward compatibility, but make it use the new method
+        public static Stock ParseCsvSingleStock(string content)
+        {
+            var stocks = ParseCsv(content);
+            return stocks.FirstOrDefault() ?? new Stock();
+        }
+
+        public static bool ValidateDateRanges(List<Stock> stocks)
+        {
+            // Minimum 2 stocks required
+            if (stocks.Count < 2) return true;
+
+            // Check if all stocks have at least same number of prices
+            var expectedCount = stocks.First().Prices.Count;
+            var allSamePriceCount = stocks.All(s => s.Prices.Count == expectedCount);
+            if (!allSamePriceCount) return false;
+
+            // Check if all stocks have the same dates as you can't compare some stocks with prices on Monday and others missing that Date even if overall they have the same count of prices
+            var firstStock = stocks.First();
+            if (!firstStock.Prices.Any()) return false;
+
+            var expectedDates = firstStock.Prices.Select(p => p.Date).OrderBy(d => d).ToList();
+
+            foreach (var stock in stocks.Skip(1))
+            {
+                var stockDates = stock.Prices.Select(p => p.Date).OrderBy(d => d).ToList();
+
+                if (!expectedDates.SequenceEqual(stockDates))
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
--- README.md
+++ README.md
@@ -7,11 +7,15 @@ The optimizer will use historical stock data to calculate the optimal allocation
 
 * Calculates key metrics for stocks from user file uploads
 * Calculates the optimal allocation of each stock that achieves maximum Sharpe Ratio for the portfolio
+* Accepts Single CSV file uploads with multiple stocks or multiple CSV files with single stock data as long as it has the same date range
 
 ## How to Use
 
 * Build and Run the blazor app in Visual Studio
 * Navigate to the Portfolio Tab
-* Upload 2 or more CSV files with stock data (You can use your own or a test file from the folder you can find in the root called 'TestData')
+* Upload 1 or more CSV files with at least 2 different stock data between all the files and has the same date range (You can use your own or a test file from the folder you can find in the root called 'TestData')
+	- Can accept either 1 single CSV but with multiple stock data
+	- Or multiple CSV files with just one stock data each
+	- Or a combination of both i.e one CSV with multiple stocks and one or more CSV with a single stock
 * The app will process the data and display key metrics for each stock in the portfolio
 * Click the button at the bottom of the page to get the optimal weights for each stock inside the portfolio
